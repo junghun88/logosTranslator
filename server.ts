1f815f024e49c708d96d2a0fc95b6ac609985fb6
junghun88 <93863527+junghun88@users.noreply.github.com>
2026-07-04 07:09:20 +0900

    feat: make server port configurable
    Update server port to use the environment variable PORT if defined, defaulting to 3000.

--- server.ts
+++ server.ts
@@ -83,7 +83,7 @@ function getCleanEnv(keyName: string): string | undefined {
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
   app.use(express.json());
 
